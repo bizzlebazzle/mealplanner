5a505971f61a060391a0bb1d915fff8e37bd8019
bizzlebazzle <20517820+bizzlebazzle@users.noreply.github.com>
2024-06-29 21:57:27 +0100

    Mongo should get the DB from env

--- config/database.php
+++ config/database.php
@@ -96,7 +96,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI'),
-            'database' => 'sample_mflix',
+            'database' => env('DB_DATABASE', 'forge'),
         ],
 
     ],
